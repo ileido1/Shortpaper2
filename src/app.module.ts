7a868c41173f6df483e636a7e2ade3d845643127
ileido1 <soycarlosluis862@gmail.com>
2022-06-20 21:32:26 -0400

    contrasena

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,11 +20,11 @@ import { DatabaseModule } from './database/database.module';
       // synchronize: true
 
       type: 'postgres',
-      host: 'ec2-52-71-23-11.compute-1.amazonaws.com',
+      host: 'ec2-23-23-182-238.compute-1.amazonaws.com',
       port: 5432,
-      username: 'fmqnsxiyndpczj',
+      username: 'xndqjgfouaonzs',
       password: 'ee78b63f31fd3f278118a72809e600edd7cf037c465939f0b1a1d87def16810b',
-      database: 'postgres',
+      database: 'dcloinfseo6j7n',
       autoLoadEntities: true,
       synchronize: true
 
